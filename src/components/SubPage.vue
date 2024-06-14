ba02c6feef488a7246c1fe986f30c84bd1b727bc
Corson-Dosch <hcorson-dosch@usgs.gov>
2024-06-14 09:46:02 -0500

    Add a bit more structure and styling to subpage

--- src/components/SubPage.vue
+++ src/components/SubPage.vue
@@ -1,11 +1,21 @@
 <template>
-    The route for this page is {{ pageRoute }}
-    <ReferencesSection />
-    <AuthorshipSection />
+    <section 
+        :id="pageRoute"
+    >
+        <div
+            class="text-container" 
+            :class="{ mobile: mobileView}"
+        >
+            The route for this page is {{ pageRoute }}. This viz is associated with the {{ projectRoute }} project.
+        </div>
+        <ReferencesSection />
+        <AuthorshipSection />
+    </section>
 </template>
 
 <script setup>
     import { useRoute } from 'vue-router'
+    import { isMobile } from 'mobile-device-detect';
 
     import ReferencesSection from '@/components/ReferencesSection.vue';
     import AuthorshipSection from '@/components/AuthorshipSection.vue';
@@ -13,7 +23,8 @@
     // global variables
     const route = useRoute()
     const pageRoute = route.params.pageRoute
-    
+    const projectRoute = route.params.projectRoute
+    const mobileView = isMobile;
 </script>
 
 <style scoped lang="scss">
