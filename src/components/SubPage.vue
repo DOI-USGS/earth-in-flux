17fb7c209e326f6d808a1b7881c309a709f74bbf
Corson-Dosch <hcorson-dosch@usgs.gov>
2024-06-14 12:26:44 -0500

    set up dynamic import of viz components

--- src/components/SubPage.vue
+++ src/components/SubPage.vue
@@ -1,13 +1,17 @@
 <template>
     <section 
-        :id="pageRoute"
+        :id="vizRoute"
     >
         <div
             class="text-container" 
             :class="{ mobile: mobileView}"
         >
-            The route for this page is {{ pageRoute }}. This viz is associated with the {{ projectRoute }} project.
+            <h1 class = 'title'>
+                {{ filteredChartContent.title }}
+            </h1>
+            The route for this page is {{ vizRoute }}. This viz is associated with the {{ projectRoute }} project.
         </div>
+        <VizComponent/>
         <ReferencesSection />
         <AuthorshipSection />
     </section>
@@ -16,15 +20,23 @@
 <script setup>
     import { useRoute } from 'vue-router'
     import { isMobile } from 'mobile-device-detect';
+    import { defineAsyncComponent } from 'vue'
 
+    import ChartGrid from '@/assets/content/ChartGrid.js';
     import ReferencesSection from '@/components/ReferencesSection.vue';
     import AuthorshipSection from '@/components/AuthorshipSection.vue';
 
     // global variables
     const route = useRoute()
-    const pageRoute = route.params.pageRoute
+    const vizRoute = route.params.vizRoute
     const projectRoute = route.params.projectRoute
     const mobileView = isMobile;
+    const chartContent = ChartGrid.chartGridItems;
+	const filteredChartContent = chartContent.filter(d => d.vizRoute === vizRoute)[0]
+
+    const VizComponent = defineAsyncComponent(() =>
+        import(`./${filteredChartContent.vizKey}.vue`)
+    )
 </script>
 
 <style scoped lang="scss">
