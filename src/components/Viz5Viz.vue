a896c087a9519a6d0a01a555e703ca35f7540465
Corson-Dosch <hcorson-dosch@usgs.gov>
2024-06-18 16:59:48 -0500

    add VizSection template to placeholder viz components

--- src/components/Viz5Viz.vue
+++ src/components/Viz5Viz.vue
@@ -1,10 +1,36 @@
 <template>
-    <section>
-        This is the Viz 5 viz!
-    </section>
+    <!---VizSection-->
+    <VizSection
+        :figures="true"
+        :fig-caption="false"
+    >
+        <!-- HEADING -->
+        <template #heading>
+            <h2>
+                This is the Viz 5 viz!
+            </h2>
+        </template>
+        <!-- FIGURES -->
+        <template #aboveExplanation>
+        </template>
+        <template #figures>
+        </template>
+        <!-- FIGURE CAPTION -->
+        <template #figureCaption>
+        </template>
+        <!-- EXPLANATION -->
+        <template #belowExplanation>
+        </template>
+    </VizSection>
 </template>
 
 <script setup>
+    import VizSection from '@/components/VizSection.vue';
+
+    // define props
+    defineProps({
+        text: { type: Object }
+    })
 </script>
 
 <style>
