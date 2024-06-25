fcd7b9ca3c5ddb8a54bb1f8ec9583fa49fa17724
Corson-Dosch <hcorson-dosch@usgs.gov>
2024-06-25 17:39:55 -0500

    use simpler d3.xml approach to load external svg

--- src/components/GlacierScanViz.vue
+++ src/components/GlacierScanViz.vue
@@ -16,17 +16,7 @@
             <p v-html="text.paragraph1" />
         </template>
         <template #figures>
-            <div id="cross_section-grid-container">
-                <cross_sectionPlot
-                    id="cross_section-svg2"
-                />
-                <svg
-                    v-bind="svgAttributes"
-                    v-html="svgContent"
-                    id="cross_section-svg"
-                >
-                </svg>
-            </div>
+            <div id="cross_section-grid-container" />
         </template>
         <!-- FIGURE CAPTION -->
         <template #figureCaption>
@@ -38,9 +28,8 @@
 </template>
 
 <script setup>
-    import { onMounted, ref } from "vue";
+    import { onMounted } from "vue";
     import * as d3 from 'd3';
-    import cross_sectionPlot from "@/assets/svgs/cross_section.svg";
     import VizSection from '@/components/VizSection.vue';
 
     // define props
@@ -48,97 +37,29 @@
         text: { type: Object }
     })
 
-    // global variables
-    const svgName = 'cross_section'
-    let svgPromise = ref()
-    let svgResult = ref()
-    let svgContent = ref()
-    let svgAttributes = ref()
-    // const {content, attributes} = parseSvg(svgName)
-
-    // console.log(content)
-    // console.log(attributes)
-    // const importedSvg = ref(null)
-
-    // onBeforeMount(async () => {
-    //     importedSvg.value = await parseSvg('cross_section')
-    // })
-    // console.log(importedSvg)
-
     // Declare behavior on mounted
     // functions called here
     onMounted(async () => {
         try {
-            await parseSvg(svgName)
-            
-            svgPromise.value.then(function(result) {
-                addSvgContent(result);
+            // Use external svg from s3
+            d3.xml("https://labs.waterdata.usgs.gov/visualizations/svgs/cross_section.svg").then(function(xml) {
+                // add svg content to DOM
+                const svgGrid = document.getElementById("cross_section-grid-container")
+                svgGrid.appendChild(xml.documentElement);
+                
+                // add id to svg
+                d3.select("#cross_section-grid-container").select("svg")
+                    .attr("id", "cross_section-svg")
+
+                // add interactivity
                 addInteractions();
-            })
+            });
         } catch (error) {
             console.error('Error during component mounting', error);
         }        
     });
 
-    // load svg from s3
-    async function parseSvg(svgString) {
-        let url = `https://labs.waterdata.usgs.gov/visualizations/svgs/${svgString}.svg`
-        try {
-            svgPromise.value = fetch(url)
-                .then((response) => response.text())
-                .then((svgText) => {
-                    const parsedSvg = new DOMParser()
-                        .parseFromString(svgText, 'image/svg+xml')
-                        .querySelector('svg')
-                    
-                    return(parsedSvg)
-                })
-            console.log('svg parsed')
-        } catch (error) {
-            console.error('Error parsing svg', error);
-        }
-
-        
-
-        // const svgElement = fetch(url)
-        //     .then((response) => response.text())
-        //     .then((svgText) => {
-        //         const parsedSvg = new DOMParser()
-        //             .parseFromString(svgText, 'image/svg+xml')
-        //             .querySelector('svg')
-                
-        //         return(parsedSvg)
-        //     })
-
-        // return(svgElement)
-
-
-        // return {
-        //     content: svgElement.innerHTML,
-        //     attributes: Object.fromEntries([...svgElement.attributes].map(a => [a.name, a.value]))
-        // }
-    }
-
-    function addSvgContent(result) {      
-        svgResult.value = result;
-        // console.log(svgResult.value.innerHTML)
-        // console.log('that was add svg content')
-
-        svgContent.value = svgResult.value.innerHTML.replaceAll(' xmlns="http://www.w3.org/2000/svg"','');
-        // console.log(svgContent.value)
-        // console.log(svgContent.value.replaceAll(' xmlns="http://www.w3.org/2000/svg"',''))
-        svgAttributes.value = Object.fromEntries([...svgResult.value.attributes].map(a => [a.name, a.value]))
-        console.log('Added svg content')
-        // return {
-        //     content: svgResult.value.innerHTML,
-        //     attributes: Object.fromEntries([...svgResult.value.attributes].map(a => [a.name, a.value]))
-        // }   
-    }
-
     function draw_xs(line_id){
-        console.log(line_id)
-        console.log(d3.select("#xs-main-" + line_id).selectAll("path"))
-        console.log(d3.select("#xs-topo-" + line_id).selectAll("path"))
         d3.select("#xs-main-" + line_id).selectAll("path")
             .style("stroke-opacity", 1.0);
         d3.select("#xs-w" + line_id).selectAll("path")
@@ -220,24 +141,15 @@
     function addInteractions() {
         // set viewbox for svg with loss function chart
         const cross_sectionSVG = d3.select("#cross_section-svg");
-        console.log('whole svg:')
-        console.log(document.getElementById("cross_section-svg2"))
-        console.log(cross_sectionSVG)
-        console.log('orig svg:')
-        console.log(d3.select("#cross_section-svg2"))
-        console.log('svg groups:')
-        console.log(cross_sectionSVG.selectAll("g"))
-        console.log('svg selection')
-        console.log(cross_sectionSVG.select("#xs-main-50").selectAll("path"))
 
         var default_xs = "50"
         draw_xs(default_xs)
 
-        // // Add interaction to loss function chart
-        // cross_sectionSVG.selectAll("g")
-        //     .on("mouseover", (event) => mouseover(event,default_xs))
-        //     .on("mouseout", (event) => mouseout(event))
-        //     .on("mouseleave", (event) => mouseleave(event,default_xs))
+        // Add interaction to svg
+        cross_sectionSVG.selectAll("g")
+            .on("mouseover", (event) => mouseover(event,default_xs))
+            .on("mouseout", (event) => mouseout(event))
+            .on("mouseleave", (event) => mouseleave(event,default_xs))
     }
 </script>
 
@@ -250,11 +162,13 @@
         grid-template-areas:
             "chart";
     }
+</style>
+<style lang="scss">
+/* css for elements added/classed w/ d3 */
     #cross_section-svg {
         grid-area: chart;
         place-self: center;
         max-height: 80%;
         max-width: 80%;
     }
-
 </style>
