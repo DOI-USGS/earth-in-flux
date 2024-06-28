9512a18b79352d7c7d7212b4626f84b4af793e28
Jaenicke, Margaret (Contractor) Ellen <mjaenicke@contractor.usgs.gov>
2024-06-28 13:51:08 -0500

    #2 MR comments

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -7,11 +7,15 @@
                 <p>Short blurb about <a href="https://labs.waterdata.usgs.gov/visualizations/index.html#/" target="_blank">VizLab</a> team / about the project</p>
             </div>
         </div>
-        <PreFooterCodeLinks :gitHubRepositoryLink="vizGitHubRepositoryLink"/>
+        <PreFooterCodeLinks :gitHubRepositoryLink="gitHubRepositoryLink"/>
 </template>
 
 <script setup>
     import PreFooterCodeLinks from "@/components/PreFooterCodeLinks.vue";
+    import { isMobile } from 'mobile-device-detect';
+    
+    const gitHubRepositoryLink = import.meta.env.VITE_APP_GITHUB_REPOSITORY_LINK;
+    const mobileView = isMobile;
 </script>
 
 <style scoped lang="scss">
