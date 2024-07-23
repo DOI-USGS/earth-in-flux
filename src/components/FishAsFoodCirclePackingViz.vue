be49d624daf786b32c7d525e6540060f6c78d2ea
Corson-Dosch <hcorson-dosch@usgs.gov>
2024-07-23 11:15:40 -0500

    fix code indentation

--- src/components/FishAsFoodCirclePackingViz.vue
+++ src/components/FishAsFoodCirclePackingViz.vue
@@ -80,26 +80,26 @@
 
         // Append the nodes.
         const node = svg.append("g")
-        .selectAll("circle")
-        .data(root.descendants().slice(1))
-        .join("circle")
-            .attr("fill", d => d.children ? color(d.depth) : "white")
-            .attr("pointer-events", d => !d.children ? "none" : null)
-            .on("mouseover", function() { d3.select(this).attr("stroke", "#000"); })
-            .on("mouseout", function() { d3.select(this).attr("stroke", null); })
-            .on("click", (event, d) => focus !== d && (zoom(event, d), event.stopPropagation()));
+            .selectAll("circle")
+            .data(root.descendants().slice(1))
+            .join("circle")
+                .attr("fill", d => d.children ? color(d.depth) : "white")
+                .attr("pointer-events", d => !d.children ? "none" : null)
+                .on("mouseover", function() { d3.select(this).attr("stroke", "#000"); })
+                .on("mouseout", function() { d3.select(this).attr("stroke", null); })
+                .on("click", (event, d) => focus !== d && (zoom(event, d), event.stopPropagation()));
 
         // Append the text labels.
         const label = svg.append("g")
             .style("font", "10px sans-serif")
             .attr("pointer-events", "none")
             .attr("text-anchor", "middle")
-        .selectAll("text")
-        .data(root.descendants())
-        .join("text")
-            .style("fill-opacity", d => d.parent === root ? 1 : 0)
-            .style("display", d => d.parent === root ? "inline" : "none")
-            .text(d => [d.data.name, d3.format("$.1s")(d.value).replace("G","B")].join("\n"));
+            .selectAll("text")
+            .data(root.descendants())
+            .join("text")
+                .style("fill-opacity", d => d.parent === root ? 1 : 0)
+                .style("display", d => d.parent === root ? "inline" : "none")
+                .text(d => [d.data.name, d3.format("$.1s")(d.value).replace("G","B")].join("\n"));
 
         // Create the zoom behavior and zoom immediately in to the initial focus node.
         svg.on("click", (event) => zoom(event, root));
@@ -108,33 +108,31 @@
         zoomTo([focus.x, focus.y, focus.r * 2]);
 
         function zoomTo(v) {
-        const k = width / v[2];
+            const k = width / v[2];
 
-        view = v;
+            view = v;
 
-        label.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
-        node.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
-        node.attr("r", d => d.r * k);
+            label.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
+            node.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
+            node.attr("r", d => d.r * k);
         }
 
         function zoom(event, d) {
-        const focus0 = focus;
+            focus = d;
 
-        focus = d;
+            const transition = svg.transition()
+                .duration(event.altKey ? 7500 : 750)
+                .tween("zoom", d => {
+                    const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
+                    return t => zoomTo(i(t));
+                });
 
-        const transition = svg.transition()
-            .duration(event.altKey ? 7500 : 750)
-            .tween("zoom", d => {
-                const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
-                return t => zoomTo(i(t));
-            });
-
-        label
-            .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
-            .transition(transition)
-            .style("fill-opacity", d => d.parent === focus ? 1 : 0)
-            .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
-            .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
+            label
+                .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
+                .transition(transition)
+                .style("fill-opacity", d => d.parent === focus ? 1 : 0)
+                .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
+                .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
         }
 
         return svg.node();
