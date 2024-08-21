4a79278ebb6ebf7c66968fa9bfef4d5d100701d1
Corson-Dosch <hcorson-dosch@usgs.gov>
2024-08-21 00:16:32 -0500

    update chart styling, explore layout options

--- src/components/FishAsFoodLinkChartViz.vue
+++ src/components/FishAsFoodLinkChartViz.vue
@@ -6,14 +6,12 @@
     >
         <!-- HEADING -->
         <template #heading>
-            <h2>
-            </h2>
         </template>
         <!-- FIGURES -->
         <template #aboveExplanation>
         </template>
         <template #figures>
-            <div id="chart-container"></div>
+            <div id="chart-container" class="maxWidth" ref="chart"></div>
         </template>
         <!-- FIGURE CAPTION -->
         <template #figureCaption>
@@ -25,7 +23,7 @@
 </template>
 
 <script setup>
-    import { onMounted, ref } from "vue";
+    import { onMounted, ref, reactive } from "vue";
     import * as d3 from 'd3';
     import VizSection from '@/components/VizSection.vue';
 
@@ -36,15 +34,22 @@
 
     // global variables
     const publicPath = import.meta.env.BASE_URL;
-    const dataFile = 'fish_as_food_climate_test.csv'
+    const dataFile = 'fish_as_food_climate.csv' //'fish_as_food_climate_test.csv'
     const data = ref();
+    const chart = ref(null);
+    // const families = ref();
     let chartDimensions;
     const chartTitle = 'Title of chart';
+    let chartSVG;
     let chartBounds;
     let xScale;
+    let xAxis;
     let yScale;
+    let yAxis;
     let widthScale;
     let colorScale;
+    // Create a reactive object to track expanded families
+    // const expandedFamilies = reactive({});
 
 
     // Behavior on mounted (functions called here)
@@ -53,13 +58,20 @@
         try {
             await loadDatasets();
             if (data.value.length > 0) {
+                // families.value = Array.from(new Set(data.value.map(d => d.family)));
+                // // Initialize the expandedFamilies object
+                // families.value.forEach(family => {
+                //     expandedFamilies[family] = false;
+                // });
+                // expandedFamilies["A"] = !expandedFamilies["A"]
+                // console.log(expandedFamilies)
                 initChart({
-                    width: 400,
-                    height: 300,
-                    margin: 10,
+                    width: chart.value.offsetWidth,
+                    height: 1600,
+                    margin: 20,
                     marginBottom: 30,
-                    marginLeft: 50});
-                drawChart();
+                    marginLeft: 200});
+                drawChart(data.value);
             } else {
                 console.error('Error loading data');
             }
@@ -111,7 +123,7 @@
         chartDimensions.boundedHeight = chartDimensions.height - chartDimensions.margin.top - chartDimensions.margin.bottom
 
         // draw canvas for chart
-        const chartSVG = d3.select("#chart-container")
+        chartSVG = d3.select("#chart-container")
             .append("svg")
                 .attr("viewBox", [0, 0, (chartDimensions.width), (chartDimensions.height)].join(' '))
                 .attr("width", "100%")
@@ -135,6 +147,9 @@
         // Initialize scales
         initXScale()
         initYScale()
+
+        // Inititalize gradients
+        initGradients()
     }
 
     function initXScale() {
@@ -143,15 +158,15 @@
             .range([0, chartDimensions.boundedWidth]);
 
         // add group for x axis
-        const xAxis = chartBounds.append("g")
+        xAxis = chartBounds.append("g")
             .attr("id", "x-axis")
             .attr("class", "axis")
             .attr("transform", `translate(0,${chartDimensions.boundedHeight})`)
             .attr("aria-hidden", true) // hide from screen reader
 
         // generate x axis
-        xAxis
-            .call(d3.axisBottom(xScale).tickSize(0).tickPadding(10))
+        // xAxis
+        //     .call(d3.axisBottom(xScale).tickSize(0).tickPadding(10))
             // .select(".domain").remove() // remove axis line
         
         // add placeholder for x axis title (title text set in drawChart())
@@ -171,14 +186,14 @@
             .padding(0.1)
 
         // add group for y axis
-        const yAxis = chartBounds.append("g")
+        yAxis = chartBounds.append("g")
             .attr("id", "y-axis")
             .attr("class", "axis")
             .attr("aria-hidden", true)
 
         // generate y axis
-        yAxis
-            .call(d3.axisLeft(yScale).tickSize(2))
+        // yAxis
+        //     .call(d3.axisLeft(yScale).tickSize(2))
             // .select(".domain").remove() // remove axis line
 
         // add placeholder for y axis title (title text set in drawChart())
@@ -190,6 +205,43 @@
             .style("text-anchor", "middle")
             .attr("role", "presentation")
             .attr("aria-hidden", true)
+    }
+
+    function initGradients() {
+        let colors = {warm: '#FF7256', cool: '#5CACEE', cold: '#36648B'}
+        Object.keys(colors).forEach(color => {
+            let lg_decreasing = chartSVG.append("defs").append("linearGradient")
+                .attr("id", color + "_gradient_decreasing")
+                .attr("x1", "0%")
+                .attr("x2", "100%")
+                .attr("y1", "0%")
+                .attr("y2", "0%");
+            lg_decreasing.append("stop")
+                .attr("offset", "40%")
+                .style("stop-color", colors[color])
+                .style("stop-opacity", 1)
+
+            lg_decreasing.append("stop")
+                .attr("offset", "100%")
+                .style("stop-color", colors[color])
+                .style("stop-opacity", 0)
+
+            let lg_increasing = chartSVG.append("defs").append("linearGradient")
+                .attr("id", color + "_gradient_increasing")
+                .attr("x1", "100%")
+                .attr("x2", "0%")
+                .attr("y1", "0%")
+                .attr("y2", "0%");
+            lg_increasing.append("stop")
+                .attr("offset", "40%")
+                .style("stop-color", colors[color])
+                .style("stop-opacity", 1)
+
+            lg_increasing.append("stop")
+                .attr("offset", "100%")
+                .style("stop-color", colors[color])
+                .style("stop-opacity", 0)
+        })
     }
 
     function initWidthScale(low, high) {
@@ -222,23 +274,49 @@
         return itemColor;
     }
 
-    function drawChart() {
+    function drawChart(data) {
         // accessor functions
         const yAccessor = d => d.species
+        // const yAccessor_family = d => d.family
         const xAccessor = d => d.cvi
+        // const xAccessor_family = d => d.cvi_family
         const x0Accessor = d => d.cvi_2030
         const x1Accessor = d => d.cvi_2075
+        const x0Accessor_family = d => d.cvi_2030_family
+        const x1Accessor_family = d => d.cvi_2075_family
         const widthAccessor = d => d.position
         const colorAccessor = d => d.thermal_guild
         const identifierAccessor = d => d.family + '_' + d.species
 
+        // to get dynamic
+        // need key for data
+        // need enter update exit pattern with transitions
+        // need to transition chart height, yscale, yaxis
+
+
         // set domain for xScale
         xScale
-            .domain([0, d3.max([d3.max(data.value, x0Accessor), d3.max(data.value, x1Accessor)])])
+            .domain([0, 0.3]) // d3.max([d3.max(data, x0Accessor), d3.max(data, x1Accessor)])])
+        xAxis
+            .call(d3.axisBottom(xScale).tickSize(0).tickPadding(10))
 
+        xAxis
+            .selectAll("text")
+            .attr("class", "axis-text")
         // set domain for yScale
+        // let yDomain = []
+        // families.value.map(family => expandedFamilies[family] ? yDomain.push(data.filter(d => d.family === family).map(d => d.species)) : yDomain.push(family))
+        // yDomain = yDomain.flat()
+        // yScale
+        //     .domain(yDomain)
         yScale
-            .domain(d3.union(data.value.map(yAccessor).sort(d3.ascending)))
+            .domain(d3.union(data.map(yAccessor))) //.sort(d3.ascending)))
+        yAxis
+            .call(d3.axisLeft(yScale).tickSize(2))
+        
+        yAxis
+            .selectAll("text")
+            .attr("class", "axis-text")
 
         // set up width scale
         const radiusPosition0 = 1
@@ -252,26 +330,35 @@
             .x(d => xScale(xAccessor(d)))
             .y0(d => yScale(yAccessor(d)) - widthScale(widthAccessor(d)))
             .y1(d => yScale(yAccessor(d)) + widthScale(widthAccessor(d)));
+            // .x(d => expandedFamilies[d.family] ? xScale(xAccessor(d)) : xScale(xAccessor_family(d)))
+            // .y0(d => expandedFamilies[d.family] ? yScale(yAccessor(d)) - widthScale(widthAccessor(d)) : yScale(yAccessor_family(d)) - widthScale(widthAccessor(d)))
+            // .y1(d => expandedFamilies[d.family] ? yScale(yAccessor(d)) + widthScale(widthAccessor(d)) : yScale(yAccessor_family(d)) + widthScale(widthAccessor(d)));
 
         // set up color scale
-        const colorCategories = Array.from(new Set(data.value.map(colorAccessor)))
+        const colorCategories = Array.from(new Set(data.map(colorAccessor)))
         initColorScale(colorCategories)
 
         // set up area data
-        const areaCategories = Array.from(new Set(data.value.map(yAccessor)))
+        const areaCategories = Array.from(new Set(data.map(yAccessor)))
         const areaData = areaCategories.map(areaCategory => {
             return [
                 {
                     species: areaCategory,
-                    cvi: data.value.filter(d => d.species === areaCategory)[0].cvi_2030,
+                    family: data.filter(d => d.species === areaCategory)[0].family,
+                    cvi: x0Accessor(data.filter(d => d.species === areaCategory)[0]),
+                    cvi_decreasing: x0Accessor(data.filter(d => d.species === areaCategory)[0]) > x1Accessor(data.filter(d => d.species === areaCategory)[0]),
+                    cvi_family: x0Accessor_family(data.filter(d => d.species === areaCategory)[0]),
                     position: 0,
-                    thermal_guild: data.value.filter(d => d.species === areaCategory)[0].thermal_guild
+                    thermal_guild: colorAccessor(data.filter(d => d.species === areaCategory)[0])
                 },
                 {
                     species: areaCategory,
-                    cvi: data.value.filter(d => d.species === areaCategory)[0].cvi_2075,
+                    family: data.filter(d => d.species === areaCategory)[0].family,
+                    cvi: x1Accessor(data.filter(d => d.species === areaCategory)[0]),
+                    cvi_decreasing: x0Accessor(data.filter(d => d.species === areaCategory)[0]) > x1Accessor(data.filter(d => d.species === areaCategory)[0]),
+                    cvi_family: x1Accessor_family(data.filter(d => d.species === areaCategory)[0]),
                     position: 1,
-                    thermal_guild: data.value.filter(d => d.species === areaCategory)[0].thermal_guild
+                    thermal_guild: colorAccessor(data.filter(d => d.species === areaCategory)[0])
                 }
             ]
         })
@@ -286,14 +373,29 @@
                     .attr("id", d => 'area-2030-' + identifierAccessor(d))
                     .attr('class', "area")
                     .attr('d', d => area(d))
-                    .attr('fill', d => colorScale(colorAccessor(d[0])))
-                    .style("opacity", 1);
+                    .attr('fill', d => d[0].cvi_decreasing ? `url(#${d[0].thermal_guild}_gradient_decreasing)`: `url(#${d[0].thermal_guild}_gradient_increasing)`)//d => colorScale(colorAccessor(d[0])))
+                    .style("opacity", 1)
+                    // .on("click", function(event, d) {
+                    //     const clickedFamily = d[0].family
+                    //     expandedFamilies[clickedFamily] = !expandedFamilies[clickedFamily]
+                    //     let yDomain = []
+                    //     families.value.map(family => expandedFamilies[family] ? yDomain.push(data.filter(d => d.family === family).map(d => d.species)) : yDomain.push(family))
+                    //     yDomain = yDomain.flat()
+                    //     const yBandwidth = yScale.bandwidth()
+                    //     chartDimensions.boundedHeight = yBandwidth * yDomain.length;
+                    //     chartDimensions.height = chartDimensions.boundedHeight + chartDimensions.margin.top + chartDimensions.margin.bottom;
+                    //     chartSVG.attr("viewBox", [0, 0, (chartDimensions.width), (chartDimensions.height)].join(' '))
+                    //     yScale.range([chartDimensions.boundedHeight * 2, 0])
+                    //     yAxis
+                    //         .call(d3.axisLeft(yScale).tickSize(2))
+                    //     drawChart(data)
+                    // });
 
         chartBounds.append("g")
             .attr("id", "points-2030")
             .attr("class", "points points_2030")    
             .selectAll('points')
-                .data(data.value)
+                .data(data)
                 .enter()
                 .append("circle")
                     .attr("id", d => 'point-2030-' + identifierAccessor(d))
@@ -308,7 +410,7 @@
             .attr("id", "points-2075")
             .attr("class", "points points_2075")    
             .selectAll('points')
-                .data(data.value)
+                .data(data)
                 .enter()
                 .append("circle")
                     .attr("id", d => 'point-2075-' + identifierAccessor(d))
@@ -324,7 +426,7 @@
         //     .attr("id", "eyes-2075")
         //     .attr("class", "eyes eyes_2075")    
         //     .selectAll('eyes')
-        //         .data(data.value)
+        //         .data(data)
         //         .enter()
         //         .append("circle")
         //             .attr("id", d => 'point-2075-' + identifierAccessor(d))
@@ -340,5 +442,10 @@
     }
 </script>
 
-<style>
+<style lang="scss">
+    .axis-text {
+        font-size: 1.8rem;
+        font-family: var(--default-font);
+        user-select: none;
+    }
 </style>
