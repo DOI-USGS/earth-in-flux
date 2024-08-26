5873cdf6b2156bcc3903bb088698ce56ac58a2aa
Corson-Dosch <hcorson-dosch@usgs.gov>
2024-08-26 16:48:46 -0500

    refine chart card styling

--- src/components/ChartCard.vue
+++ src/components/ChartCard.vue
@@ -37,90 +37,85 @@
 </script>
 
 <style scoped lang="scss">
+    $collapsed-height: 280px;
+    $expanded-height: 400px;
+    $height-difference: $expanded-height - $collapsed-height;
 	.chart{
 		display: flex;
 		flex-direction: column-reverse;
 		justify-content: flex-start;
 		position: relative;
-		width: 280px;
-		height: 28rem;//100%;
+		width: $collapsed-height;
+		height: $collapsed-height;
         -moz-transition: height 1s ease;
         -webkit-transition: height 1s ease;
         -o-transition: height 1s ease;
         transition: height 1s ease;
-        // overflow: hidden;
-        background-color: white;
-        // align-items: start;
+        background-color: rgb(255, 255, 255);
+        box-shadow: 0px 0px 8px rgba(39,44,49,.07), 1px 4px 4px rgba(39,44,49,.04);
 	}
     .chart:hover {
-        // transform: scale(1.1);
-        height: 370px;
+        height: $expanded-height;
         transform: scaleY(1);
-        // overflow: auto;
-    }
-
-    .overlay {
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: rgba(0, 0, 0, 0.5);
-        color: white;
-        opacity: 0;
-        display: flex;
-        font-weight: 700;
-    }
-
-    .chart:hover .overlay {
-        opacity: 1;
     }
     .takeaway {
         position: absolute;
-        top: 230px;
         left: 0;
         right: 0;
-        bottom: 0;
-        line-height: 1;
-        display: inline-block;
-        vertical-align: middle;
-        -webkit-transition: top 1s ease-out; 
-        -moz-transition: top 1s ease-out; 
-        -ms-transition: top 1s ease-out; 
-        -o-transition: top 1s ease-out; 
-        transition: top 1s ease-out;
+        bottom: calc($height-difference / 2);
+        line-height: 2rem;
+        -webkit-transition: top 1s ease; 
+        -moz-transition: top 1s ease; 
+        -ms-transition: top 1s ease; 
+        -o-transition: top 1s ease; 
+        transition: top 1s ease;
     }
     .takeaway p {
-        padding: 0.2rem 2rem 0.2rem 2rem; 
+        transform: translateY(50%);
+        padding: 0.2rem 2rem 0.2rem 2rem;
+        opacity: 0;
+        transition: all 1s ease; 
     }
-    .chart:hover .takeaway {
-        top: 300px;
+    .chart:hover .takeaway p {
+        opacity: 1;
     }
     .chart img {
         position: absolute;
         width: 100%;
         top: 0;
+        background-color: white;
+        transition: all 1s ease; 
     }
-
-    .overlay p {
-        margin: 0;
-        padding: 10px;
-        text-align: center;
+    .chart:hover img {
+        transform: translate3D(0,-0.5px,0) scale(1.05); 
+        box-shadow:  0px 0px 8px rgba(39,44,49,.07), 4px 8px 4px rgba(39,44,49,.1), 1px 4px 4px rgba(39,44,49,.07);
     }
     .project {
+        font-family: sans-serif; /* This is fallback font for old browsers */
+        font-family: var(--title-font);
+        color: var(--color-title-text);
         position: absolute;
-        top: calc(280px - 2rem);
+        top: $collapsed-height;
         width: auto;
-        // left: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        -webkit-transition: top 1s ease; 
+        -moz-transition: top 1s ease; 
+        -ms-transition: top 1s ease; 
+        -o-transition: top 1s ease; 
+        transition: top 1s ease;
+        background-color: var(--faded-usgs-blue);
+        border-radius: 0.25rem 0.25rem 0 0;
+        box-shadow: 1px 4px 4px rgba(107, 107, 107, 0.1);
     }
     .project p {
-        background-color: var(--medium-grey);
-        padding: 0.2rem 0.5rem 0.2rem 0.5rem; 
+        font-size: 1.4rem;
+        padding: 0.9rem 1.5rem 0.9rem 1.5rem; 
         text-align: center;
         width: auto;
-        font-weight: 200;
-        border: solid var(--color-background);
-        border-radius: 10% 10% 0% 0%;
-        // color: var(--color-background)
+        font-weight: 400;
+    }
+    .chart:hover .project {
+        top: $expanded-height;
     }
 </style>
