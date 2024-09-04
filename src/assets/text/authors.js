25e7c57dcfd61eef63d2060286a2167740aaec8c
Althea A Archer <aaarcher@usgs.gov>
2024-09-04 08:04:36 -0500

    authorship for beaufort sea pages

--- src/assets/text/authors.js
+++ src/assets/text/authors.js
@@ -155,7 +155,7 @@ export default {
       {
         firstName: 'Althea',
         lastName: 'Archer',
-        fullName: 'Althea Anne Archer',
+        fullName: 'Althea A. Archer',
         initials: 'AA',
         profile_link: 'https://www.usgs.gov/staff-profiles/althea-archer',
         role: 'lead author',
@@ -178,6 +178,72 @@ export default {
         fullName: 'Hayley Corson-Dosch',
         initials: 'HCD',
         profile_link: 'https://www.usgs.gov/staff-profiles/hayley-corson-dosch',
+        role: 'lead developer',
+        contribution: 'co-led the design and development of the website'
+      }
+    ]
+  },
+  BeaufortSeaCore: {
+    primaryAuthors: [
+      {
+        firstName: 'Althea',
+        lastName: 'Archer',
+        fullName: 'Althea A. Archer',
+        initials: 'AAA',
+        profile_link: 'https://www.usgs.gov/staff-profiles/althea-archer',
+        role: 'lead author',
+        contribution: 'led the creation of this page, created illustrations and animations, and wrote the narrative'
+      },
+    ],
+    additionalAuthors: [
+      {
+        firstName: 'Maggie',
+        lastName: 'Jaenicke',
+        fullName: 'Maggie Jaenicke',
+        initials: 'MJ',
+        profile_link: 'https://www.usgs.gov/staff-profiles/margaret-maggie-jaenicke',
+        role: 'lead developer',
+        contribution: 'co-led the design and development of the website and created illustrations'
+      },
+      {
+        firstName: 'Hayley',
+        lastName: 'Corson-Dosch',
+        fullName: 'Hayley Corson-Dosch',
+        initials: 'HCD',
+        profile_link: 'https://www.usgs.gov/staff-profiles/hayley-corson-dosch',
+        role: 'lead developer',
+        contribution: 'co-led the design and development of the website'
+      }
+    ]
+  },
+  BeaufortSeaTimeline: {
+    primaryAuthors: [
+      {
+        firstName: 'Hayley',
+        lastName: 'Corson-Dosch',
+        fullName: 'Hayley Corson-Dosch',
+        initials: 'HCD',
+        profile_link: 'https://www.usgs.gov/staff-profiles/hayley-corson-dosch',
+        role: 'lead author',
+        contribution: 'led the creation of the interactive chart and co-led the design and development of the website'
+      },
+    ],
+    additionalAuthors: [
+      {
+        firstName: 'Althea',
+        lastName: 'Archer',
+        fullName: 'Althea A. Archer',
+        initials: 'AAA',
+        profile_link: 'https://www.usgs.gov/staff-profiles/hayley-corson-dosch',
+        role: 'lead developer',
+        contribution: 'analyzed the data, conceptualized the interactive chart, and wrote the narrative'
+      },
+      {
+        firstName: 'Maggie',
+        lastName: 'Jaenicke',
+        fullName: 'Maggie Jaenicke',
+        initials: 'MJ',
+        profile_link: 'https://www.usgs.gov/staff-profiles/margaret-maggie-jaenicke',
         role: 'lead developer',
         contribution: 'co-led the design and development of the website'
       }
