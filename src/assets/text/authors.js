a8ef69639aee794ec6380b8393941b7ee40dece5
Corson-Dosch <hcorson-dosch@usgs.gov>
2024-10-17 13:59:55 -0500

    clean up authorship

--- src/assets/text/authors.js
+++ src/assets/text/authors.js
@@ -1,257 +1,124 @@
 export default {
-  TEMPLATE: {
-    // do not delete section. delete individuals as needed. modify content as needed
-    // currently only 'fullName', 'firstName', 'initials', 'profile_link', and 'contribution' are used
-    primaryAuthors: [
-      {
-        firstName: 'Hayley',
-        lastName: 'Corson-Dosch',
-        fullName: 'Hayley Corson-Dosch',
-        initials: 'HCD',
-        profile_link: 'https://www.usgs.gov/staff-profiles/hayley-corson-dosch',
-        
-        role: 'lead developer',
-        contribution: 'co-led the design and development of the website'
-      },
-      {
-        firstName: 'Maggie',
-        lastName: 'Jaenicke',
-        fullName: 'Maggie Jaenicke',
-        initials: 'MJ',
-        profile_link: 'https://www.usgs.gov/staff-profiles/margaret-maggie-jaenicke',
-        role: 'lead developer',
-        contribution: 'co-led the design and development of the website and also created illustrations'
-      },
-      {
-        firstName: 'Cee',
-        lastName: 'Nell',
-        fullName: 'Cee Nell',
-        initials: 'CN',
-        profile_link: 'https://www.usgs.gov/staff-profiles/cee-nell',
-        role: 'team lead',
-        contribution: 'acted in an advisory role, contributed to website development, and reviewed code'
-      }
-    ],
-    // do not delete section. delete any or all individuals as needed. modify content as needed
-    // currently only 'fullName', 'firstName', 'initials', 'profile_link', and 'contribution' are used
-    additionalAuthors: [
-      {
-        firstName: 'Althea',
-        lastName: 'Archer',
-        fullName: 'Althea A. Archer',
-        initials: 'AAA',
-        profile_link: 'https://www.usgs.gov/staff-profiles/althea-archer',
-        role: 'contributor',
-        contribution: 'created data visualizations'
-      },
-      {
-        firstName: 'Elmera',
-        lastName: 'Azadpour',
-        fullName: 'Elmera Azadpour',
-        initials: 'EA',
-        profile_link: 'https://www.usgs.gov/staff-profiles/elmera-azadpour',
-        role: 'contributor',
-        contribution: 'created data visualizations'
-      },
-      {
-        firstName: 'Jeffrey',
-        lastName: 'Kwang',
-        fullName: 'Jeffrey Kwang',
-        initials: 'JK',
-        profile_link: 'https://www.usgs.gov/staff-profiles/jeffrey-kwang',
-        role: 'contributor',
-        contribution: 'created data visualizations'
-      },
-      {
-        firstName: 'Kaysa',
-        lastName: 'Vaarre-Lamoureux',
-        fullName: 'Kaysa Vaarre-Lamoureux',
-        initials: 'KVL',
-        profile_link: null,
-        role: 'contributor',
-        contribution: 'created data visualizations'
-      }
-    ]
-  },
-  GlacierScan: {
-    primaryAuthors: [
-      {
-        firstName: 'Jeffrey',
-        lastName: 'Kwang',
-        fullName: 'Jeffrey Kwang',
-        initials: 'JK',
-        profile_link: 'https://www.usgs.gov/staff-profiles/jeffrey-kwang',
-        role: 'contributor',
-        contribution: 'led the development of the data visualization'
-      },
-    ],
-    additionalAuthors: [
-    ]
-  },
-  Aerosols: {
-    primaryAuthors: [
-      {
-        firstName: 'Jeffrey',
-        lastName: 'Kwang',
-        fullName: 'Jeffrey Kwang',
-        initials: 'JK',
-        profile_link: 'https://www.usgs.gov/staff-profiles/jeffrey-kwang',
-        role: 'contributor',
-        contribution: 'led the development of the data visualization'
-      },
-    ],
-    additionalAuthors: [
-    ]
-  },
-  ThreatBumpChart: {
-    primaryAuthors: [
-      {
-        firstName: 'Cee',
-        lastName: 'Nell',
-        fullName: 'Cee Nell',
-        initials: 'CN',
-        profile_link: 'https://www.usgs.gov/staff-profiles/cee-nell',
-        role: 'team lead',
-        contribution: 'led the development of the data visualization'
-      }
-    ],
-    additionalAuthors: [
-    ]
-  },
-  BeaufortSeaSpecies: {
-    primaryAuthors: [
-      {
-        firstName: 'Althea',
-        lastName: 'Archer',
-        fullName: 'Althea A. Archer',
-        initials: 'AA',
-        profile_link: 'https://www.usgs.gov/staff-profiles/althea-archer',
-        role: 'lead author',
-        contribution: 'led the development of the data visualization, created illustrations, and wrote the narrative'
-      },
-      {
-        firstName: 'Maggie',
-        lastName: 'Jaenicke',
-        fullName: 'Maggie Jaenicke',
-        initials: 'MJ',
-        profile_link: 'https://www.usgs.gov/staff-profiles/margaret-maggie-jaenicke',
-        role: 'lead developer',
-        contribution: 'created illustrations'
-      }
-    ],
-    additionalAuthors: [
-    ]
-  },
-  BeaufortSeaCore: {
-    primaryAuthors: [
-      {
-        firstName: 'Althea',
-        lastName: 'Archer',
-        fullName: 'Althea A. Archer',
-        initials: 'AA',
-        profile_link: 'https://www.usgs.gov/staff-profiles/althea-archer',
-        role: 'lead author',
-        contribution: 'led the development of the data visualization, created illustrations, and wrote the narrative'
-      },
-      {
-        firstName: 'Maggie',
-        lastName: 'Jaenicke',
-        fullName: 'Maggie Jaenicke',
-        initials: 'MJ',
-        profile_link: 'https://www.usgs.gov/staff-profiles/margaret-maggie-jaenicke',
-        role: 'lead developer',
-        contribution: 'created illustrations'
-      }
-    ],
-    additionalAuthors: [
-    ]
-  },
-  BeaufortSeaTimeline: {
-    primaryAuthors: [
-      {
-        firstName: 'Hayley',
-        lastName: 'Corson-Dosch',
-        fullName: 'Hayley Corson-Dosch',
-        initials: 'HCD',
-        profile_link: 'https://www.usgs.gov/staff-profiles/hayley-corson-dosch',
-        role: 'lead author',
-        contribution: 'led the creation of the interactive chart'
-      },
-      {
-        firstName: 'Althea',
-        lastName: 'Archer',
-        fullName: 'Althea A. Archer',
-        initials: 'AAA',
-        profile_link: 'https://www.usgs.gov/staff-profiles/hayley-corson-dosch',
-        role: 'lead developer',
-        contribution: 'analyzed the data, conceptualized the interactive chart, and wrote the narrative'
-      },
-    ],
-    additionalAuthors: [
-    ]
-  },
-  FishAsFoodSankey: {
-    primaryAuthors: [
-      {
-        firstName: 'Hayley',
-        lastName: 'Corson-Dosch',
-        fullName: 'Hayley Corson-Dosch',
-        initials: 'HCD',
-        profile_link: 'https://www.usgs.gov/staff-profiles/hayley-corson-dosch',
-        
-        role: 'lead developer',
-        contribution: 'built this data visualization'
-      },
-    ],
-    additionalAuthors: [
-    ]
-  },
-  FishAsFoodCirclePacking: {
-    primaryAuthors: [
-      {
-        firstName: 'Hayley',
-        lastName: 'Corson-Dosch',
-        fullName: 'Hayley Corson-Dosch',
-        initials: 'HCD',
-        profile_link: 'https://www.usgs.gov/staff-profiles/hayley-corson-dosch',
-        
-        role: 'lead developer',
-        contribution: 'built this data visualization'
-      },
-    ],
-    additionalAuthors: [
-    ]
-  },
-  FishAsFoodLinkChart: {
-    primaryAuthors: [
-      {
-        firstName: 'Hayley',
-        lastName: 'Corson-Dosch',
-        fullName: 'Hayley Corson-Dosch',
-        initials: 'HCD',
-        profile_link: 'https://www.usgs.gov/staff-profiles/hayley-corson-dosch',
-        
-        role: 'lead developer',
-        contribution: 'built this data visualization'
-      },
-    ],
-    additionalAuthors: [
-    ]
-  },
-  ThreatSankey: {
-    primaryAuthors: [
-      {
-        firstName: 'Hayley',
-        lastName: 'Corson-Dosch',
-        fullName: 'Hayley Corson-Dosch',
-        initials: 'HCD',
-        profile_link: 'https://www.usgs.gov/staff-profiles/hayley-corson-dosch',
-        
-        role: 'lead developer',
-        contribution: 'built this data visualization'
-      },
-    ],
-    additionalAuthors: [
-    ]
-  }
+  TEMPLATE: [
+    {
+      fullName: 'Hayley Corson-Dosch',
+      initials: 'HCD',
+      profile_link: 'https://www.usgs.gov/staff-profiles/hayley-corson-dosch'
+    },
+    {
+      fullName: 'Maggie Jaenicke',
+      initials: 'MJ',
+      profile_link: 'https://www.usgs.gov/staff-profiles/margaret-maggie-jaenicke'
+    },
+    {
+      fullName: 'Cee Nell',
+      initials: 'CN',
+      profile_link: 'https://www.usgs.gov/staff-profiles/cee-nell'
+    },
+    {
+      fullName: 'Althea A. Archer',
+      initials: 'AA',
+      profile_link: 'https://www.usgs.gov/staff-profiles/althea-archer'
+    },
+    {
+      fullName: 'Elmera Azadpour',
+      initials: 'EA',
+      profile_link: 'https://www.usgs.gov/staff-profiles/elmera-azadpour'
+    },
+    {
+      fullName: 'Jeffrey Kwang',
+      initials: 'JK',
+      profile_link: 'https://www.usgs.gov/staff-profiles/jeffrey-kwang'
+    },
+    {
+      fullName: 'Kaysa Vaarre-Lamoureux',
+      initials: 'KVL',
+      profile_link: null
+    }
+  ],
+  GlacierScan: [
+    {
+      fullName: 'Jeffrey Kwang',
+      initials: 'JK',
+      profile_link: 'https://www.usgs.gov/staff-profiles/jeffrey-kwang'
+    },
+  ],
+  Aerosols: [
+    {
+      fullName: 'Jeffrey Kwang',
+      initials: 'JK',
+      profile_link: 'https://www.usgs.gov/staff-profiles/jeffrey-kwang'
+    },
+  ],
+  ThreatBumpChart: [
+    {
+      fullName: 'Cee Nell',
+      initials: 'CN',
+      profile_link: 'https://www.usgs.gov/staff-profiles/cee-nell'
+    }
+  ],
+  BeaufortSeaSpecies: [
+    {
+      fullName: 'Althea A. Archer',
+      initials: 'AA',
+      profile_link: 'https://www.usgs.gov/staff-profiles/althea-archer'
+    },
+    {
+      fullName: 'Maggie Jaenicke',
+      initials: 'MJ',
+      profile_link: 'https://www.usgs.gov/staff-profiles/margaret-maggie-jaenicke'
+    }
+  ],
+  BeaufortSeaCore: [
+    {
+      fullName: 'Althea A. Archer',
+      initials: 'AA',
+      profile_link: 'https://www.usgs.gov/staff-profiles/althea-archer'
+    },
+    {
+      fullName: 'Maggie Jaenicke',
+      initials: 'MJ',
+      profile_link: 'https://www.usgs.gov/staff-profiles/margaret-maggie-jaenicke'
+    }
+  ],
+  BeaufortSeaTimeline: [
+    {
+      fullName: 'Hayley Corson-Dosch',
+      initials: 'HCD',
+      profile_link: 'https://www.usgs.gov/staff-profiles/hayley-corson-dosch'
+    },
+    {
+      fullName: 'Althea A. Archer',
+      initials: 'AA',
+      profile_link: 'https://www.usgs.gov/staff-profiles/althea-archer'
+    },
+  ],
+  FishAsFoodSankey: [
+    {
+      fullName: 'Hayley Corson-Dosch',
+      initials: 'HCD',
+      profile_link: 'https://www.usgs.gov/staff-profiles/hayley-corson-dosch'
+    },
+  ],
+  FishAsFoodCirclePacking: [
+    {
+      fullName: 'Hayley Corson-Dosch',
+      initials: 'HCD',
+      profile_link: 'https://www.usgs.gov/staff-profiles/hayley-corson-dosch'
+    },
+  ],
+  FishAsFoodLinkChart: [
+    {
+      fullName: 'Hayley Corson-Dosch',
+      initials: 'HCD',
+      profile_link: 'https://www.usgs.gov/staff-profiles/hayley-corson-dosch'
+    },
+  ],
+  ThreatSankey: [
+    {
+      fullName: 'Hayley Corson-Dosch',
+      initials: 'HCD',
+      profile_link: 'https://www.usgs.gov/staff-profiles/hayley-corson-dosch'
+    },
+  ]
 };
