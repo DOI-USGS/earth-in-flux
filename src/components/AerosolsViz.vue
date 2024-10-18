fc120a44fb4a018326293cb5290267c6f9f12d21
Jeffrey Kwang <jkwang@usgs.gov>
2024-10-18 18:24:50 -0500

    aerosols - simplified viz and new thumbnail

--- src/components/AerosolsViz.vue
+++ src/components/AerosolsViz.vue
@@ -42,7 +42,7 @@
     onMounted(async () => {
         try {
             // Use external svg from s3
-            d3.xml("https://labs.waterdata.usgs.gov/visualizations/svgs/aerosols.svg").then(function(xml) {
+            d3.xml("https://labs.waterdata.usgs.gov/visualizations/svgs/aerosols_map.svg").then(function(xml) {
                 // add svg content to DOM
                 const svgGrid = document.getElementById("aerosols-grid-container")
                 svgGrid.appendChild(xml.documentElement);
@@ -60,56 +60,96 @@
     });
 
 
-    function draw_trajectories(fire){
-        for(let trajectory_num=1;trajectory_num<=24;trajectory_num++){
-            d3.select("#trajectory-" + fire + "-" + trajectory_num).selectAll("path")
-                .style("stroke-opacity", 0.4);
-            var trajectory_line = d3.select("#trajectory-" + fire + "-" + trajectory_num).selectAll("path");
+    function draw_trajectories(fire,trajectory_num,smoke_lines,delay,duration){
+        for(let smoke_num=0;smoke_num<smoke_lines;smoke_num++){
+            var trajectory_line = d3.select("#trajectory-" + fire + "-" + trajectory_num + "-" + smoke_num).selectAll("path");
             var totalLength = trajectory_line.node().getTotalLength();
+            var opacity = 0.6;
+            
             // Animate the line
             trajectory_line.style("transition", "none")
                 .style("stroke-dasharray", totalLength + " " + totalLength)
-                .style("stroke-dashoffset", totalLength)
+                .style("stroke-dashoffset", totalLength - 1) //minus 1 remove artifacts that show before animation.
+                .style("stroke-opacity", opacity)
                 .transition()
-                .delay(200*(trajectory_num-1))
-                .duration(2000)
+                .delay(delay)
+                .duration(duration)
                 .ease(d3.easeLinear)
                 .style("stroke-dashoffset", 0);
-
-            d3.select('#wildfire-label-'+fire).select("text")
-                .style("opacity", 1.0);
         }
     }
 
-    function remove_trajectories(fire){
-        for(let trajectory_num=1;trajectory_num<=24;trajectory_num++){
-            d3.select("#trajectory-" + fire + "-" + trajectory_num).selectAll("path")
-                .style("stroke-opacity", 0.0);
-            d3.select('#wildfire-label-'+fire).select("text")
+    function remove_trajectories(fire,trajectory_num,smoke_lines){
+        for(let smoke_num=0;smoke_num<smoke_lines;smoke_num++){
+            d3.select("#trajectory-" + fire + "-" + trajectory_num + "-" + smoke_num).selectAll("path")
+                .style("stroke-opacity", 0.0)
+                .interrupt();
+        }
+    }
+
+    function mouseover(event,number_of_fires,smoke_lines) {
+        if (event.currentTarget.id.startsWith("source-")){
+            for (let fire=0;fire<number_of_fires;fire++){
+                    d3.select('#wildfire-label-'+fire).selectAll("path")
+                        .style("opacity", 0.0);
+                    d3.select('#wildfire-label-'+fire).selectAll("text")
+                        .style("opacity", 0.0);
+            }
+            d3.select('#multi-path-label').selectAll("text")
                 .style("opacity", 0.0);
+            d3.select('#multi-path-label').selectAll("path")
+                .style("opacity", 0.0);
+            let fire = event.currentTarget.id.slice(7);
+            for(let trajectory_num=1;trajectory_num<=24;trajectory_num++){
+                draw_trajectories(fire,trajectory_num,smoke_lines,0.0,2700);//300*(trajectory_num-1),2700);
+            }
         }
     }
 
-    function highlight_core(coreid, corealpha, corecolor){
-        console.log(coreid)
-        d3.select("#"+coreid).selectAll("path")
-            .style("stroke-opacity", corealpha)
-            .style("stroke", corecolor);
-    }
-
-    function mouseover(event,coreids) {
+    function mouseout(event,number_of_fires,smoke_lines) {
         if (event.currentTarget.id.startsWith("source-")){
+            for (let fire=0;fire<number_of_fires;fire++){
+                    d3.select('#wildfire-label-'+fire).selectAll("path")
+                        .style("opacity", 0.75);
+                    d3.select('#wildfire-label-'+fire).selectAll("text")
+                        .style("opacity", 1.0);
+            }
+            d3.select('#multi-path-label').selectAll("path")
+                .style("opacity", 0.75);
+            d3.select('#multi-path-label').selectAll("text")
+                .style("opacity", 1.0);
             let fire = event.currentTarget.id.slice(7);
-            draw_trajectories(fire);
-            highlight_core(coreids[fire], 1.0, "#d62728");
+            for(let trajectory_num=1;trajectory_num<=24;trajectory_num++){
+                remove_trajectories(fire,trajectory_num,smoke_lines);
+            }
         }
     }
 
-    function mouseout(event,coreids) {
-        if (event.currentTarget.id.startsWith("source-")){
-            let fire = event.currentTarget.id.slice(7);
-            remove_trajectories(fire);
-            highlight_core(coreids[fire], 0.2, "#000000");
+    function mouseenter(event,default_fire,number_of_fires,default_smoke_num,smoke_lines) {
+        if (event.currentTarget.id.startsWith("figure_1")){
+            d3.select('#single-path-label').selectAll("path")
+                .style("opacity", 0.0);
+            d3.select('#single-path-label').selectAll("text")
+                .style("opacity", 0.0);
+            d3.select('#multi-path-label').selectAll("path")
+                .style("opacity", 0.75);
+            d3.select('#multi-path-label').selectAll("text")
+                .style("opacity", 1.0);
+            remove_trajectories(default_fire,default_smoke_num,smoke_lines)
+        }
+    }
+
+    function mouseleave(event,default_fire,number_of_fires,default_smoke_num,smoke_lines) {
+        if (event.currentTarget.id.startsWith("figure_1")){
+            d3.select('#single-path-label').selectAll("path")
+                .style("opacity", 0.75);
+            d3.select('#single-path-label').selectAll("text")
+                .style("opacity", 1.0);
+            d3.select('#multi-path-label').selectAll("text")
+                .style("opacity", 0.0);
+            d3.select('#multi-path-label').selectAll("path")
+                .style("opacity", 0.0);
+            draw_trajectories(default_fire,default_smoke_num,smoke_lines,0,1000)
         }
     }
 
@@ -117,11 +157,38 @@
         // set viewbox for svg with loss function chart
         const aerosolsSVG = d3.select("#aerosols-svg");
 
-        const coreids = ["id-2017-Core-1","id-2016-Core-3"]
+        // draw default line
+        const default_fire = 1;
+        const default_smoke_num = 8;
+        const smoke_lines = 1;
+        const number_of_fires = 2;
+        for (let fire=0;fire<number_of_fires;fire++){
+            d3.select('#wildfire-label-'+fire).selectAll("path")
+                .style("opacity", 0.75);
+            d3.select('#wildfire-label-'+fire).selectAll("text")
+                .style("opacity", 1.0);
+        }
+        d3.select('#JIF-label').selectAll("path")
+            .style("opacity", 0.75);
+        d3.select('#JIF-label').selectAll("text")
+            .style("opacity", 1.0);
+        d3.select('#single-path-label').selectAll("path")
+            .style("opacity", 0.75);
+        d3.select('#single-path-label').selectAll("text")
+            .style("opacity", 1.0);
+        d3.select('#multi-path-label').selectAll("path")
+            .style("opacity", 0.0);
+        d3.select('#multi-path-label').selectAll("text")
+            .style("opacity", 0.0);
+
+        draw_trajectories(default_fire,default_smoke_num,smoke_lines,0,1000)
+
         // Add interaction to loss function chart
         aerosolsSVG.selectAll("g")
-            .on("mouseover", (event) => mouseover(event,coreids))
-            .on("mouseout", (event) => mouseout(event,coreids));
+            .on("mouseover", (event) => mouseover(event,number_of_fires,smoke_lines))
+            .on("mouseout", (event) => mouseout(event,number_of_fires,smoke_lines))
+            .on("mouseenter", (event) => mouseenter(event,default_fire,number_of_fires,default_smoke_num,smoke_lines))
+            .on("mouseleave", (event) => mouseleave(event,default_fire,number_of_fires,default_smoke_num,smoke_lines));
     }
 </script>
 
@@ -135,6 +202,7 @@
             "chart";
     }
 </style>
+
 <style lang="scss">
 /* css for elements added/classed w/ d3 */
     #aerosols-svg {
@@ -142,5 +210,20 @@
         place-self: center;
         max-height: 100%;
         max-width: 100%;
+    }
+</style>
+
+<style>
+    #wildfire-label-0 {
+        cursor: default;
+    }
+    #wildfire-label-1 {
+        cursor: default;
+    }
+    #single-path-label {
+        cursor: default;
+    }
+    #multi-path-label {
+        cursor: default;
     }
 </style>
