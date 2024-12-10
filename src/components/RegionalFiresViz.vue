dc926435f3ba6c0f7ade97f1edb2368a69d7b899
Corson-Dosch <hcorson-dosch@usgs.gov>
2024-12-09 21:50:07 -0600

    fix regional fires svg height issue

--- src/components/RegionalFiresViz.vue
+++ src/components/RegionalFiresViz.vue
@@ -201,7 +201,7 @@
     }
 
     function addInteractions() {
-        // set viewbox for svg with loss function chart
+        // set width and height of svg
         const aerosolsSVG = d3.select("#aerosols-svg")
             .attr("width", "100%")
             .attr("height", "100%");
@@ -288,7 +288,8 @@
     #aerosols-svg {
         grid-area: chart;
         place-self: center;
-        max-height: 100%;
+        height: 100%;
+        max-height: 85vh;
         max-width: 100%;
     }
 </style>
